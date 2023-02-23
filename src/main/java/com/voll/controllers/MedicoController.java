d240ac92da8d20e0d1006b67a41f775a12e4e07a
Willian Bogler Da Silva <willian.silva@pti.org.br>
2023-02-22 21:25:18 -0300

    ?size = 5; ?sort = nome ou crm; ?size=5&sort = crm -> formas de usar na url

--- src/main/java/com/voll/controllers/MedicoController.java
+++ src/main/java/com/voll/controllers/MedicoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,7 +37,7 @@ public class MedicoController {
 	}
 	
 	@GetMapping("/listar")
-	public Page<Object> listarMedicos(Pageable paginacao){
+	public Page<Object> listarMedicos(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao){
 		return medicoRepository.findAll(paginacao).map(ListagemMedicos::new);
 
 	}
